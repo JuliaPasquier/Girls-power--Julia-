8d1e9ccae7593cd06f4cc9e63a032e44861f9cad
Naïké Dramé <naike.drame@gmail.com>
2023-11-30 16:53:14 +0100

    Started to update routes for rendering

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,14 +1,14 @@
 //Controller actions
 module.exports.dashboard_get = (req, res) => {
-    res.send("Dashboard Page");
+    res.render("index");
 };
 
 module.exports.signup_get = (req, res) => {
-    res.send("signup page");
+    res.render("register");
 };
 
 module.exports.login_get = (req, res) => {
-    res.send("login page");
+    res.render("login");
 };
 
 module.exports.signup_post = (req, res) => {
@@ -20,19 +20,23 @@ module.exports.login_post = (req, res) => {
 };
 
 module.exports.logout_get = (req, res) => {
-    res.send("successfully logged out");
+    res.render("login");
 };
 
 module.exports.profile_get = (req, res) => {
-    res.send("profile page");
+    res.render("profile");
 };
 
 module.exports.create_get = (req, res) => {
-    res.send("create offer page");
+    res.send("create-offer");
 };
 
 module.exports.create_post = (req, res) => {
     res.send("successfully added a new offer");
+};
+
+module.exports.edit_get = (req, res) => {
+    res.render("update-offer");
 };
 
 module.exports.update_put = (req, res) => {
