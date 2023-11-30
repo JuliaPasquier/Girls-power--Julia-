69025ff89e6833c2d0c9da268d9f95399029bdb0
Dystrima <138579225+Dystrima@users.noreply.github.com>
2023-11-30 16:04:22 +0100

    Update Offers.js

--- models/Offers.js
+++ models/Offers.js
@@ -1,1 +1,64 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+
+const employerSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: [true, "Please enter your email"],
+		unique: true,
+		lowercase: true,
+		validate: [isEmail, "Please enter a valid email"],
+		trim: true,
+	},
+	phone: {
+		type: String,
+		trim: true,
+	},
+	address: {
+		type: String,
+		trim: true,
+	},
+});
+
+const offerSchema = new mongoose.Schema({
+	createdAt: {
+		type: Date,
+		immutable: true,
+		default: () => Date.now(),
+	},
+	updatedAt: {
+		type: Date,
+		default: () => Date.now(),
+	},
+	jobTitle: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	url: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	employer: employerSchema,
+	offerOrigin: {
+		type: String,
+		required: true,
+	},
+	offerStatus: {
+		type: String,
+		required: true,
+	},
+	comments: {
+		type: String,
+	},
+	// User ID ?
+});
+
+const Offer = mongoose.model("offer", offerSchema);
+module.exports = Offer;
