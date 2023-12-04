7ad433dc0bb1b5a6426685aae3221004c99422c8
Julia <pasquierjulia5@gmail.com>
2023-12-04 16:49:22 +0100

    errors

--- routes/router.js
+++ routes/router.js
@@ -1,32 +1,32 @@
 const { Router } = require("express");
 const controller = require("../controllers/controller");
-const { requireAuth } = require("../middleware/authMiddleware");
+// const { requireAuth } = require("../middleware/authMiddleware");
 
 const router = Router();
 
 // General
-router.get("/", requireAuth, controller.dashboard_get);
+router.get("/", controller.dashboard_get);
 
 // Register
 router.get("/signup", controller.signup_get);
 router.post("/signup", controller.signup_post);
 
 // Login
-router.get("/login", requireAuth, controller.login_get);
+router.get("/login", controller.login_get);
 router.post("/login", controller.login_post);
 
 // Logout
 router.get("/logout", controller.logout_get);
 
 // Profile
-router.get("/profile", requireAuth, controller.profile_get);
+router.get("/profile",  controller.profile_get);
 
 // Create
-router.get("/create", requireAuth,controller.create_get);
-router.post("/create", requireAuth, controller.create_post);
+router.get("/create", controller.create_get);
+router.post("/create", controller.create_post);
 
 // Update
-router.get("/update", requireAuth, controller.update_get);
-router.put("/update", requireAuth, controller.update_put);
+router.get("/update", controller.update_get);
+router.put("/update", controller.update_put);
 
 module.exports = router;
