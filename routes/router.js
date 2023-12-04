e4bd9087babf363363edc8b20a825a1b684e74fb
Julia <pasquierjulia5@gmail.com>
2023-12-04 09:24:31 +0100

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -4,14 +4,19 @@ const controller = require("../controllers/controller");
 const router = Router();
 
 router.get("/", controller.dashboard_get);
+
 router.get("/signup", controller.signup_get);
 router.post("/signup", controller.signup_post);
+
 router.get("/login", controller.login_get);
 router.post("/login", controller.login_post);
+
 router.get("/logout", controller.logout_get);
 router.get("/profile", controller.profile_get);
+
 router.get("/create", controller.create_get);
 router.post("/create", controller.create_post);
+
 router.get("/update", controller.update_get);
 router.put("/update", controller.update_put);
 
